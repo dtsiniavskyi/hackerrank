fd88b846a0d4edbe34dd340137fa384bd8fa6bf5
Dmytro <dmytro.tsiniavskyi@gmail.com>
2022-02-15 16:22:55 +0200

    [+] add task text to challenge 1 and 2

--- python/Introduction/001-hello-world.py
+++ python/Introduction/001-hello-world.py
@@ -1,1 +1,10 @@
+# Here is a sample line of code that can be executed in Python:
+
+# print("Hello, World!")
+# You can just as easily store a string as a variable and then print it to stdout:
+
+# my_string = "Hello, World!"
+# print(my_string)
+# The above code will print Hello, World! on your screen. 
+
 print('Hello World!')
