c622b1e3e6ff4b8ff1a4b41e62ab4bab8bff4afb
Dmytro <dmytro.tsiniavskyi@gmail.com>
2022-02-17 15:23:38 +0200

    [+] initate challange scrapping

---
+++ Scrapper/Scrapper.Cli/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Asastra.Extensions.HtmlAgilityPack;
+using HtmlAgilityPack;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Scrapper.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string className = "hackdown-content";
+            const string taskUrl = "https://www.hackerrank.com/challenges/list-comprehensions/problem?isFullScreen=true";
+
+            // TODO: OCR text from svg ...
+
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(taskUrl);
+
+            var html = driver.FindElement(By.ClassName(className)).GetAttribute("innerHTML");
+
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            var paragraphs = doc.DocumentNode.Descendants().Where(e => e.Name == "p");
+
+            foreach (var p in paragraphs)
+                Console.WriteLine(p.InnerText);
+        }
+    }
+}
